106c48a705aed36e8629956728b487e418f63a06
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-01-25 20:03:18 -0700

    Remove redundant Card::new()

--- server/src/card.rs
+++ server/src/card.rs
@@ -18,11 +18,6 @@ pub struct Card(pub Suit, pub u8); // ace = 0, king = 12
 impl Card {
     pub const RANK_RANGE: Range<u8> = 0..12;
 
-    pub fn new(suit: Suit, rank: u8) -> Card {
-        debug_assert!(rank >= 0 && rank <= 12);
-        Card(suit, rank)
-    }
-
     pub fn serialize(&self) -> u8 {
         self.0 as u8 | (self.1 << 2)
     }
